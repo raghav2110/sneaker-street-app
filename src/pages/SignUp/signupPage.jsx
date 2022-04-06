076533b31125f7f95608fc2d3f53bc938e622cb8
Raghav Heda <raghav.heda@elastic.run>
2022-04-06 18:56:56 +0530

    [feat]: CSS fix

--- src/pages/SignUp/signupPage.jsx
+++ src/pages/SignUp/signupPage.jsx
@@ -12,22 +12,22 @@ export function SignUp(){
                     <form className="form-area">
                         <div>
                             <label for="Name" className="email-label">Name</label>
-                            <input type="" id="Name" placeholder="Enter Your Name" class="email-input" />
+                            <input type="email" id="Name" placeholder="Enter Your Name" class="email-input-field" />
                         </div>
                         <div>
                             <label for="Email" className="email-label">Email</label>
-                            <input type="email" id="Email" placeholder="youremail@gmail.com" class="email-input" />
+                            <input type="email" id="Email" placeholder="youremail@gmail.com" class="email-input-field" />
                         </div>
                         <div className="password-wrapper">
                             <label for="Password" className="email-label">Password</label>
-                            <input type="password" id="Password" placeholder="Your Password" class="email-input" />
+                            <input type="password" id="Password" placeholder="Your Password" class="email-input-field" />
                             <div className="toggle-icon">
                                 <span><i className="fa fa-eye-slash fa-lg"></i></span>
                             </div>
                         </div>
                         <div className="password-wrapper">
                             <label for="ConfirmPass" className="email-label">Confirm Password</label>
-                            <input type="password" id="ConfirmPass" placeholder="Enter Same Password" class="email-input" />
+                            <input type="password" id="ConfirmPass" placeholder="Enter Same Password" class="email-input-field" />
                         </div>
                         <div className="button-wrapper">
                             <button type="submit" className="btn btn-primary btn-login">Sign Up</button>
