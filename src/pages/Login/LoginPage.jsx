bd89d0945230a97e15cc6f3dfdd1879dd93f2619
Raghav Heda <raghav.heda@elastic.run>
2022-04-06 18:23:13 +0530

    [feat]: CSS fix

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -13,11 +13,11 @@ export function Login(){
                         <form className="form-area">
                             <div>
                                 <label for="Email" className="email-label">Email</label>
-                                <input type="email" id="Email" placeholder="youremail@gmail.com" className="email-input" />
+                                <input type="email" id="Email" placeholder="youremail@gmail.com" class="email-input" />
                             </div>
                             <div className="password-wrapper">
                                 <label for="Password" className="email-label">Password</label>
-                                <input type="password" id="Password" placeholder="Your Password" className="email-input" />
+                                <input type="password" id="Password" placeholder="Your Password" class="email-input" />
                                 <div className="toggle-icon">
                                     <span><i className="fa fa-eye-slash fa-lg"></i></span>
                                 </div>
--- src/pages/SignUp/signupPage.jsx
+++ src/pages/SignUp/signupPage.jsx
@@ -5,29 +5,29 @@ export function SignUp(){
     return(
         <>
         <Navbar />
-        <div className="main-wrapper">
+        <div className="main-wrapper-login">
             <div className="login-wrapper">
                 <div className="login-text">Sign Up</div>
                 <div className="input-area">
                     <form className="form-area">
                         <div>
                             <label for="Name" className="email-label">Name</label>
-                            <input type="" id="Name" placeholder="Enter Your Name" className="email-input" />
+                            <input type="" id="Name" placeholder="Enter Your Name" class="email-input" />
                         </div>
                         <div>
                             <label for="Email" className="email-label">Email</label>
-                            <input type="email" id="Email" placeholder="youremail@gmail.com" className="email-input" />
+                            <input type="email" id="Email" placeholder="youremail@gmail.com" class="email-input" />
                         </div>
                         <div className="password-wrapper">
                             <label for="Password" className="email-label">Password</label>
-                            <input type="password" id="Password" placeholder="Your Password" className="email-input" />
+                            <input type="password" id="Password" placeholder="Your Password" class="email-input" />
                             <div className="toggle-icon">
                                 <span><i className="fa fa-eye-slash fa-lg"></i></span>
                             </div>
                         </div>
                         <div className="password-wrapper">
                             <label for="ConfirmPass" className="email-label">Confirm Password</label>
-                            <input type="password" id="ConfirmPass" placeholder="Enter Same Password" className="email-input" />
+                            <input type="password" id="ConfirmPass" placeholder="Enter Same Password" class="email-input" />
                         </div>
                         <div className="button-wrapper">
                             <button type="submit" className="btn btn-primary btn-login">Sign Up</button>
